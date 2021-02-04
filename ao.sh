43de01bd9b8027a5a52ba55ba39e4a3e82d37cf6
khalolm8 <69640746+khalolm8@users.noreply.github.com>
2021-02-04 23:59:26 +0100

    Add files via upload

---
+++ ao.sh
@@ -0,0 +1,1 @@
+ffmpeg -stream_loop -1 -i "ao.mp4" -lavfi 'scale=1280:720' -deinterlace -vcodec libx264 -pix_fmt yuv420p -preset veryfast -r 30 -g 60 -b:v 2500k -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k -f flv "rtmps://live-api-s.facebook.com:443/rtmp/106475288060371?s_bl=1&s_ps=1&s_psm=1&s_sw=0&s_vt=api-s&a=Abxaw02T3mKtcc1v"
